c0fc2d57886c6c0bc2dc1ee0b06c1ef046227708
TanNgoc <factyel.bttn@gmail.com>
2023-05-09 23:05:55 +0700

    init app

---
+++ hotel-management-api/hotel-management-api/hotel-management-api/APIs/User/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel_management_api.APIs.User
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("/api/v{version:apiVersion}/user")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok("Get method");
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("post method");
+        }
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("put method");
+        }
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("dedlete method");
+        }
+    }
+}
